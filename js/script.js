10631b92bceb5bbea752011ba90d78c01c8a4256
Novemberman90 <alexledovit@gmail.com>
2025-02-21 03:28:22 +0200

    add script from addon__template

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+window.addEventListener('DOMContentLoaded',()=>{
+
+
 const menuBtn = document.querySelector('.menu__btn');
 const menuBlock = document.querySelector('.menu__block');
 
@@ -18,3 +21,42 @@ const openMenu = () => {
   document.body.classList.toggle('lock');
   menuBlock.classList.toggle('menu__block--active');
 }
+
+
+
+const startAddonNumber = (elements)=> {
+  const time = 2000;  // Общее время анимации в миллисекундах
+  const step = 1; // Шаг увеличения числа
+
+    elements.forEach(item => {
+      
+      let n = 0; // Начальное значение счётчик
+      let num = parseInt(item.dataset.addonNum) || 0; // значение в атрибуте 
+      let t = Math.round(time / num); // Вычисляем интервал обновления числа
+
+      let interval = setInterval(() => {
+        n += step; // Увеличиваем текущее число на 1
+        item.innerHTML = n; // Обновляю текст в элементе
+
+        if (n >= num) { //Остановка, когда достигнем нужного числа
+          clearInterval(interval)
+        } 
+      }, t);
+      
+    });
+    
+}
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const elements = document.querySelectorAll('.addon__number');
+                startAddonNumber(elements);
+                observer.disconnect(); // Отключаем после первого срабатывания
+            }
+        });
+    }, { threshold: 1.0 });
+
+    observer.observe(document.querySelector('.addon__template')); // Слежу за нужным или любым другим элементом в конце страницы
+
+})
+
