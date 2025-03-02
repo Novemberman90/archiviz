8c786164da11d37936aebef39212617d5d45a52d
Novemberman90 <alexledovit@gmail.com>
2025-03-03 01:07:11 +0200

    init gulp

--- app/js/script.js
+++ app/js/script.js
@@ -1,33 +1,31 @@
 window.addEventListener('DOMContentLoaded',()=>{
 
-
 const menuBtn = document.querySelector('.menu__btn');
 const menuBlock = document.querySelector('.menu__block');
 const goTop = document.querySelector('.go-top');
 
+/* Кнопка открытия и закрытия меню */
 menuBtn.addEventListener('click', ()=>{
   menuBtn.classList.toggle('menu__btn--active');
   openMenu()
 });
 
-const toTop = ()=>{
-  const header = document.getElementById('header');
-  
-  goTop.addEventListener('click', (e)=>{
-    e.preventDefault();
-    const top = header.getBoundingClientRect().top + window.scrollY;
-    window.scrollTo({
-      top: top,
-      behavior: "smooth"
-    });
-  })
-}
+/* Кнопка НА ВЕРХ */
+goTop.addEventListener('click', (e)=>{
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+})
 
+/* Открытие меню */
 const openMenu = () => {
   document.body.classList.toggle('lock');
   menuBlock.classList.toggle('menu__block--active');
 }
 
+/* При скроле меняется цвет хедера и активация кнопки НА ВЕРХ */
 const headerScroll = () => {
   const headerWrapper = document.querySelector('.header__wrapper');
   const headerHeight = headerWrapper.offsetHeight;
@@ -36,7 +34,6 @@ const headerScroll = () => {
   if(scrollPosition > headerHeight + 50 ){
     headerWrapper.classList.add('header__wrapper--active');
     goTop.classList.add('go-top--active');
-    toTop();
   } else {
      headerWrapper.classList.remove('header__wrapper--active');
      goTop.classList.remove('go-top--active');
@@ -45,10 +42,11 @@ const headerScroll = () => {
 }
 window.addEventListener('scroll', headerScroll);
 
+/* Счетчик */
 const startAddonNumber = (elements)=> {
   const time = 2000;  // Общее время анимации в миллисекундах
 
-    elements.forEach(item => {
+   /*  elements.forEach(item => {
       
       let n = 0; // Начальное значение счётчик
       let num = parseInt(item.dataset.addonNum) || 0; // значение в атрибуте 
@@ -62,9 +60,27 @@ const startAddonNumber = (elements)=> {
         } 
       }, stepTime);
       
-    });
+    }); */
     
+    elements.forEach(item => {
+        let startTime;
+        let num = parseInt(item.dataset.addonNum) || 0;
+
+        const updateNumber = (timestamp) => {
+            if (!startTime) startTime = timestamp;
+            let progress = Math.min((timestamp - startTime) / time, 1);
+            item.textContent = Math.floor(progress * num);
+
+            if (progress < 1) {
+                requestAnimationFrame(updateNumber);
+            }
+        };
+
+        requestAnimationFrame(updateNumber);
+    });
 }
+
+/* Слежу когда счетчик попадёт в поле видимости, чтобы его запустить */
 const addonElement = document.querySelector('.addon');
   if (addonElement) {
     const observer = new IntersectionObserver((entries, observer) => {
